defea70bfe965ba8f13b0949af5da3f9bb73a5af
Lucas Eduardo <lucasatletico@hotmail.com>
2021-09-19 00:53:08 -0300

    Create the method Delete e patch

--- Controllers/EmployeesController.cs
+++ Controllers/EmployeesController.cs
@@ -38,5 +38,28 @@ namespace APICrud.Controllers
             return Created(HttpContext.Request.Scheme + "://" + HttpContext.Request.Host + HttpContext.Request.Path + "/" + employee.Id,
                 employee);
         }
+
+        [HttpDelete]
+        [Route("api/[controller]/{id}")]
+        public IActionResult DeleteEmployee(Guid id) {
+            var employee = _employeeData.GetEmployee(id);
+            if(employee != null) {
+                _employeeData.DeleteEmployee(employee);
+                return Ok();
+            }
+            return NotFound($"Employee with id: {id} was not found");
+        }
+
+        [HttpPatch]
+        [Route("api/[controller]/{id}")]
+        public IActionResult EditEmployee(Guid id, Employee employee) {
+            var existingEmployee = _employeeData.GetEmployee(id);
+            if (existingEmployee != null) {
+                employee.Id = existingEmployee.Id;
+                _employeeData.DeleteEmployee(employee);    
+            }
+
+            return Ok(employee);
+        }
     }
 }
