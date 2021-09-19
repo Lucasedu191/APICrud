b1f570886a8498724424c1f9a541eeda41597e4e
Lucas Eduardo <lucasatletico@hotmail.com>
2021-09-18 23:10:27 -0300

    Create the class Employee, and the methods of Crud

---
+++ EmployeeData/IEmployeeData.cs
@@ -0,0 +1,20 @@
+﻿
+using APICrud.Models;
+using System;
+using System.Collections.Generic;
+
+namespace APICrud.EmployeeData
+{
+    interface IEmployeeData
+    {
+        List<Employee> GetEmployees();
+
+        Employee GetEmployee(Guid id);
+
+        Employee AddEmployee(Employee employee);
+
+        void DeleteEmployee(Employee employee);
+
+        Employee EditEmployee(Employee employee);
+    }
+}
